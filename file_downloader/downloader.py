6e41f4e9b32bf8164dcb853d6fcdd2faabed722e
Seyoung Park <seyoung.arts.park@protonmail.com>
2018-10-02 11:02:56 +0300

    Add scripts

--- file_downloader/downloader.py
+++ file_downloader/downloader.py
@@ -4,8 +4,11 @@ urls = []
 with open("filelinks") as f:
     urls = f.readlines()
 
+
+
 # Download the file from `url` and save it locally under `file_name`:
-for url in urls:
-    with urllib.request.urlopen(url) as response, open(url.split('/')[-1], 'wb') as out_file:
-        data = response.read() # a `bytes` object
-        out_file.write(data)
+for url in urls[19:]:
+	file_name = "downloads/" + url.split('filename=')[-1]
+	with urllib.request.urlopen(url) as response, open(file_name, 'wb') as out_file:
+		data = response.read() # a `bytes` object
+		out_file.write(data)
